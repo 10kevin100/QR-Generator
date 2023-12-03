7d24cbb0a3f56e8ef33cb49f043576f84bab6d15
Kevin Moran <kevin.portillo01@gmail.com>
2023-12-02 21:07:14 -0600

    QRCODE INICIO
    INICIO

--- admin/src/Components/QrCode.js
+++ admin/src/Components/QrCode.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import QRCode from 'react-qr-code';
 
 const QrCode = () => {
-  return (
-    <div>
-      QRCODE
-    </div>
-  )
-}
+    const [text, setText] = useState('');
 
-export default QrCode
+    function handlechange(e) {
+        const newText = e.target.value;
+        setText(newText);
+    }
+
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="bg-gray-200 p-6 rounded-lg text-center">
+                <h1 className="text-3xl mb-4">Generador de código QR</h1>
+                <div className="flex justify-center">
+                    <div style={{ alignSelf: 'center' }}>
+                        <QRCode
+                            value={text}
+                            bgColor="#ffffff"
+                            fgColor="#000000"
+                            level="L"
+                            size={256}
+                        />
+                    </div>
+                </div>
+                <div className="input-here mt-4">
+                    <h4 className="text-sm text-gray-600">
+                        Ingresa la URL para crear el código QR
+                    </h4>
+                    <input type="text" value={text} onChange={(e) => handlechange(e)} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default QrCode;
