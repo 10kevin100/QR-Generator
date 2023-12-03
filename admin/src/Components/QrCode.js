3e90ee7bb3da5de970408e75079d9922cdac909d
Kevin Moran <kevin.portillo01@gmail.com>
2023-12-02 19:58:54 -0600

    Creacion de aplicaciones
    Creacion de aplicacion QR Generator

---
+++ admin/src/Components/QrCode.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QrCode = () => {
+  return (
+    <div>
+      QRCODE
+    </div>
+  )
+}
+
+export default QrCode
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+const port = 5000;
+
+app.listen(port,()=>{
+    console.log( `Server running on port ${port}`)
+});
+
